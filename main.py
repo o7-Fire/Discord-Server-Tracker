c63d4e3da0589d88204df244f8ddba382825dddc
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-06-15 16:09:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import discord
+import time
+import os
+from discord import Webhook, RequestsWebhookAdapter
 
 TOKEN = ""
 client = discord.Client()
+webhookadapter = RequestsWebhookAdapter()
 servertotrack = {}
 
 def updatelist():
-	with open("server-data.txt", "r+") as f:
+	with open("server-data.txt", "r") as f:
 		for line in f.read().split("\n"):
 			if not line.strip(): continue #thank you one line code
 			linfo = line.split(" ")
 			infotosave = {}
-			for i in range(len(linfo) - 1):
+			for i in range(len(linfo) - 2):
 				if i % 2 == 0:
-					infotosave[linfo[i]] = linfo[i+1]
+					infotosave[linfo[i+1]] = linfo[i+2]
 			servertotrack[linfo[0]] = infotosave
 updatelist()
 
@@ -34,8 +38,27 @@ async def on_ready():
 async def on_message(message):
 	#if message.author.bot:
 	#	return
+	#this handles the message copying process
+	if str(message.guild.id) in servertotrack:
+		print(message)
+		serverval = servertotrack[str(message.guild.id)]
+		channelwebhook = serverval[str(message.channel.id)]
+		webhook = Webhook.from_url(channelwebhook, adapter=webhookadapter)
+		if message.attachments:
+			files = []
+			for attachment in message.attachments:
+				if attachment.size < 8000000:
+					await attachment.save(fp=f"./temp/{attachment.filename}")
+					files.append(discord.File(f"./temp/{attachment.filename}"))
+					os.remove(f"./temp/{attachment.filename}")
+			webhook.send(username=str(message.author), avatar_url=message.author.avatar_url, content=message.content, files=files)
+		else:
+			webhook.send(username=str(message.author), avatar_url=message.author.avatar_url, content=message.content)
+			
+	#after this line only client can run
 	if message.author.id != client.user.id:
 		return
+	
 	if message.content.startswith("copyserver "):
 		idtocopy = int(message.content.replace("copyserver ", ""))
 		guildtocopy = client.get_guild(idtocopy)
@@ -49,9 +72,12 @@ async def on_message(message):
 				if newchannel.type == discord.ChannelType.text:
 					webhook = await newchannel.create_webhook(name="webhook name here")
 					messagetowrite += f"{str(channel.id)} {webhook.url} "
+					print(f"{str(channel.id)} {webhook.url} ")
 				
-		with open("server-data.txt", "w+") as f:
+		with open("server-data.txt", "a+") as f:
 			f.write(messagetowrite + "\n")
+		print(messagetowrite)
+		time.sleep(5)
 		updatelist()
 				
 	if message.content.startswith("exec"):
